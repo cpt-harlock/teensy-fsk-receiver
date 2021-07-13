6c849c4d035b2321939312ed89343169ee1f4d78
Andrea Monterubbiano <andrea.monterubbiano@gmail.com>
2021-07-13 16:26:05 +0200

    implemented n-fsk

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,13 @@ using namespace TeensyTimerTool;
  */
 #define SAMPLING_FREQUENCY (500.0)
 #define FFT_LENGTH (512)
-#define FREQUENCY_ONE (247.0703)
-#define FREQUENCY_ZERO (232.4219)
 #define FREQUENCY_CHIRP (72.7778)
-#define SYMBOL_FREQUENCY_INTERVAL (2)
+#define STARTING_FREQUENCY (135.0)
+#define FSK_DEVIATION (31.6484)
+//NUMBER_OF_SYMBOLS must be power of 2
+#define NUMBER_OF_SYMBOLS (4)
+#define BITS_PER_SYMBOL (2)
+#define SYMBOL_FREQUENCY_INTERVAL (15)
 #define CHIRP_MAGNITUDE_THRESHOLD (14.0)
 #define CHIRP_MAGNITUDE_INTERVAL (2)
 #define BYTES_PER_MESSAGE (3)
@@ -33,18 +36,20 @@ boolean receivingData = false;
 float32_t chirpOffset;
 float32_t chirpNorm;
 float32_t maxCorrelationValue;
-arm_rfft_fast_instance_f32 fftInstance;
-uint16_t freq_one_bin;
-uint16_t freq_zero_bin;
+//TODO: deleting freq_..._bin breaks FFT
+// uint16_t freq_one_bin;
+// uint16_t freq_zero_bin;
+uint8_t hello;
 uint16_t freq_chirp_bin;
+uint16_t frequencies_bins[NUMBER_OF_SYMBOLS];
 uint16_t maxMagnitudeBufferIndex = 0;
 float32_t maxMagnitudeBuffer = 0.0;
-uint8_t receivedData[BYTES_PER_MESSAGE+1];
+uint8_t receivedData[BYTES_PER_MESSAGE + 1];
 uint8_t uartBuffer[128];
 volatile boolean sampleAvailable = false;
 
-      
 Timer t1;
+arm_rfft_fast_instance_f32 fftInstance;
 
 void adc_start(uint8_t mux, uint8_t aref);
 
@@ -60,19 +65,17 @@ void setup()
   Serial.begin(115200); //reading as fast as possible
   Serial2.begin(115200);
   memset(chirpSamplesBuffer, 0, sizeof(float32_t) * FFT_LENGTH);
-  memset(workingChirpSamplesBuffer,0, sizeof(float32_t) * FFT_LENGTH);
+  memset(workingChirpSamplesBuffer, 0, sizeof(float32_t) * FFT_LENGTH);
   arm_rfft_fast_init_f32(&fftInstance, FFT_LENGTH);
-  freq_one_bin = round((FREQUENCY_ONE * FFT_LENGTH) / SAMPLING_FREQUENCY);
-  freq_zero_bin = round((FREQUENCY_ZERO * FFT_LENGTH) / SAMPLING_FREQUENCY);
   freq_chirp_bin = round((FREQUENCY_CHIRP * FFT_LENGTH) / SAMPLING_FREQUENCY);
+  for (size_t i = 0; i < NUMBER_OF_SYMBOLS; i++)
+  {
+    frequencies_bins[i] = round(((STARTING_FREQUENCY + i * FSK_DEVIATION) * FFT_LENGTH) / SAMPLING_FREQUENCY);
+  }
   analogReadRes(ADC_RESOLUTION);
   analogReadAveraging(ADC_SAMPLE_AVERAGE);
   pinMode(DEBUG_PIN, OUTPUT);
   t1.beginPeriodic(timerCallback, 2'000);
-  Serial.printf("Frequency one bin: %u\r\n",freq_one_bin);
-  Serial.printf("Frequency zero bin: %u\r\n",freq_zero_bin);
-  Serial.printf("Frequency chirp bin: %u\r\n",freq_chirp_bin);
-  delay(5000);
 }
 
 void loop()
@@ -108,33 +111,47 @@ void loop()
           }
         }
         Serial.printf("Max bin index: %u\r\n", maxMagnitudeBufferIndex);
-        Serial.printf("Freq one bin index: %u\r\n", freq_one_bin);
-        Serial.printf("Freq zero index: %u\r\n", freq_zero_bin);
+        for (size_t i = 0; i < NUMBER_OF_SYMBOLS; i++)
+        {
+          if (abs(frequencies_bins[i] - maxMagnitudeBufferIndex) <= SYMBOL_FREQUENCY_INTERVAL)
+          {
+            Serial.printf("Receiving %u\r\n", i);
+            //TODO: max 256 symbols
+            for (size_t j = 0; j < BITS_PER_SYMBOL; j++)
+            {
+              uint8_t symbol = i & (1u << (BITS_PER_SYMBOL - j - 1));
+              if (symbol)
+              {
+                receivedData[symbolByteCounter] |= 1 << (7 - symbolBitCounter - j);
+              }
+              else
+              {
+                receivedData[symbolByteCounter] &= ~(1 << (7 - symbolBitCounter - j));
+              }
+            }
+            break;
+          }
+        }
 
-        if (abs(maxMagnitudeBufferIndex-freq_one_bin) <= SYMBOL_FREQUENCY_INTERVAL)
-        {
-          //TODO: check bit ordering
-          receivedData[symbolByteCounter] |= 1 << (7 - symbolBitCounter);
-          Serial.println("Received one");
-        }
-        else
-        {
-          receivedData[symbolByteCounter] &= ~(1 << (7 - symbolBitCounter));
-          Serial.println("Received zero");
-        }
-        symbolBitCounter++;
+        symbolBitCounter += BITS_PER_SYMBOL;
         //if reached end of byte
-        if (symbolBitCounter == 8)
+        if (symbolBitCounter >= 8)
         {
-          symbolBitCounter = 0;
-          symbolByteCounter++;
+          symbolByteCounter += symbolBitCounter / 8;
+          symbolBitCounter = symbolBitCounter % 8;
+          Serial.printf("Byte %u: %u\r\n", symbolByteCounter, receivedData[symbolByteCounter-1]);
           if (symbolByteCounter == BYTES_PER_MESSAGE)
           {
             receivingData = 0;
             //clear chirp buffer
-            memset(chirpSamplesBuffer,0,sizeof(float32_t)*FFT_LENGTH);
+            memset(chirpSamplesBuffer, 0, sizeof(float32_t) * FFT_LENGTH);
             receivedData[BYTES_PER_MESSAGE] = 0;
-            Serial.println((char*)receivedData);
+            Serial.println((char *)receivedData);
+            memset(receivedData,0,BYTES_PER_MESSAGE+1);
+            for (size_t i = 0; i < NUMBER_OF_SYMBOLS; i++)
+            {
+              Serial.printf("Bin %d: %d\r\n", i, frequencies_bins[i]);
+            }
           }
         }
       }
@@ -169,22 +186,24 @@ void loop()
         workingChirpSamplesBuffer[i] /= chirpNorm;
       }
       //fft
-      arm_rfft_fast_f32(&fftInstance,workingChirpSamplesBuffer,fftBuffer,0);
-      arm_cmplx_mag_f32(fftBuffer,magnitudeBuffer,FFT_LENGTH);
+      arm_rfft_fast_f32(&fftInstance, workingChirpSamplesBuffer, fftBuffer, 0);
+      arm_cmplx_mag_f32(fftBuffer, magnitudeBuffer, FFT_LENGTH);
       //find max magnitude value and index
-      for (size_t i = 0; i < FFT_LENGTH/2; i++)
+      for (size_t i = 0; i < FFT_LENGTH / 2; i++)
       {
-       if(magnitudeBuffer[i] > maxMagnitudeBuffer) {
-         maxMagnitudeBuffer = magnitudeBuffer[i];
-         maxMagnitudeBufferIndex = i;
-       } 
+        if (magnitudeBuffer[i] > maxMagnitudeBuffer)
+        {
+          maxMagnitudeBuffer = magnitudeBuffer[i];
+          maxMagnitudeBufferIndex = i;
+        }
       }
-      
+
       // Serial.printf("Max magnitude %f\r\n", maxMagnitudeBuffer);
       // Serial.printf("Max magnitude index %u\r\n", maxMagnitudeBufferIndex);
       // Serial.printf("Chirp bin: %u\r\n", freq_chirp_bin);
-      if(abs(maxMagnitudeBufferIndex - freq_chirp_bin) <= CHIRP_MAGNITUDE_INTERVAL && 
-      maxMagnitudeBuffer >= CHIRP_MAGNITUDE_THRESHOLD) {
+      if (abs(maxMagnitudeBufferIndex - freq_chirp_bin) <= CHIRP_MAGNITUDE_INTERVAL &&
+          maxMagnitudeBuffer >= CHIRP_MAGNITUDE_THRESHOLD)
+      {
         receivingData = true;
         symbolBufferIndex = 0;
         symbolBitCounter = 0;
